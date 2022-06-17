c514404478be3101eaff6923bffdaafb61f20c2b
Michael <MichaelOluwayemi10@gmail.com>
2022-06-17 14:07:04 +0100

    Sign up page created

--- getgadgets.com/codehouse/js/script.js
+++ getgadgets.com/codehouse/js/script.js
@@ -4,3 +4,19 @@
 function notReady() {
     alert("This page is currently offline, please try again later, thank you!!!");
 }
+
+//password visibility
+
+function showPassword() {
+    var x = document.getElementById("password");
+    var y = document.getElementById("eyeControl");
+    if (x.type === "password") {
+        x.type = "text";
+        y.className = "fa fa-eye-slash";
+    } else {
+        x.type = "password";
+        y.className = "fa fa-eye"
+    }
+
+
+}
