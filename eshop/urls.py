91dd2e45abf184782dadb651da166c2e8d0209a9
Warith Adetayo <warithadetayo.awa@gmail.com>
2022-06-22 19:35:05 +0100

    Update to Registration FOrm

--- eshop/urls.py
+++ eshop/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", view=views.homepage, name='homepage'),
     path("sign-up", view=views.register, name='register'),
-    path("login", view=views.login, name='login')
+    path("login", view=views.userlogin, name='login'),
+    path("logout", view=views.userlogout, name='logout'),
+    path("dashboard/", view=views.dashboard, name='dashboard')
 ]
--- eshop/views.py
+++ eshop/views.py
@@ -1,24 +1,55 @@
 from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
 from .models import Category
 from .forms import CreateUserForm
+from .decorators import unauthenticated_user
 
 # Create your views here.
 
+@unauthenticated_user
 def homepage(request):
+    print(dir(request))
+    print(dir(request.user))
     categories = Category.objects.all()
     context = {"product_category":categories}
     return render(request, 'eshop/homepage.html', context)
 
+@unauthenticated_user
 def register(request):
     form = CreateUserForm()
     if request.method == "POST":
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('homepage')
+
+            name = form.cleaned_data.get('username')
+            messages.success(request, message="New User (%s) created" %(name))
+            return redirect('login')
     
     context = {"form":form}
     return render(request, 'eshop/registration-form.html', context)
 
-def login(request):
+@unauthenticated_user
+def userlogin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user != None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.info(request, "Username or Password is incorrect")
     return render(request, 'eshop/login-form.html')
+
+def userlogout(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
+def dashboard(request, pk=None):
+    return render(request, 'eshop/dashboard.html')
